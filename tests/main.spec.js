36aba495115eacec8dca5c6268f523e1080fafe2
Pedro Cruz <cruz.pedro22.11@gmail.com>
2018-08-28 23:42:36 -0300

    Adding CLI options

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,2 +1,38 @@
+import { expect } from 'chai';
+import { exec } from 'child_process';
+
+const btcConverter = './src/main.js';
+
 describe('Main CLI', () => {
+  it('should return version of btc-converter', (done) => {
+    exec(`${btcConverter} --version`, (err, stdout, stderr) => {
+      if(err) throw err;
+      expect(stdout.replace('\n', '')).to.be.equal(pkg.version);
+      done();
+    });
+  });
+
+  it('should return the description when btc-converter --help', (done) => {
+    exec(`${btcConverter} --help`, (err, stdout, stderr) => {
+      if(err) throw err;
+      expect(stdout.includes('Convert Bitcoint to any currency defined')).to.be.true;
+      done();
+    });
+  });
+
+  it('should return the currency option when btc-converter --help', (done) => {
+    exec(`${btcConverter} --help`, (err, stdout, stderr) => {
+      if(err) throw err;
+      expect(stdout.includes('--currency')).to.be.true;
+      done();
+    });
+  });
+
+  it('should return the amount option when btc-converter --help', (done) => {
+    exec(`${btcConverter} --help`, (err, stdout, stderr) => {
+      if(err) throw err;
+      expect(stdout.includes('--amount')).to.be.true;
+      done();
+    });
+  });
 });
